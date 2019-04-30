e0b3eb8db2273280d437158cfe0e391e0b95dcd1
JienanChen <47368928+JienanChen@users.noreply.github.com>
2019-04-30 15:19:36 -0600

    Transfered (April 30, 2019)

--- PlaceEnemies Version 2/sketch.js
+++ PlaceEnemies Version 2/sketch.js
@@ -1,15 +1,65 @@
-// Project Title
-// Your Name
-// Date
+// Second Version of PlaceEnemies()
+// Jienan Chen
+// April 2, 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+let gridSize = 3;
+let grid;
+let cellSize;
+let autoPlay;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+  
+  grid = createRandom2DArray(gridSize, gridSize);
+  cellSize = width/gridSize;
 }
 
 function draw() {
-  background(220);
+  background(255);
+  displayGrid();
 }
+
+function displayGrid() {
+  for (let y = 0; y < gridSize; y++) {
+    for (let x = 0; x < gridSize; x++) {      
+      
+      if (grid[y][x] === 0) {
+        fill(255);
+      }
+      else{
+        fill(0);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+
+
+function createRandom2DArray(cols, rows) {
+  let emptyArray = [];
+  let options = [[1,0,0],[0,1,0], [0,0,1]];
+  let choice = random(options);
+  let choice2 = random(options);
+  let choice3 = random(options);
+  for (let i = 0; i < rows; i++) {
+    emptyArray.push([]);
+    for (let j = 0; j < cols ; j++) {
+      emptyArray[i].push(choice[i]);
+      emptyArray[i].push(choice2[i]);
+      emptyArray[i].push(choice3[i]);
+      }
+    }
+  return emptyArray;
+}
+
+
