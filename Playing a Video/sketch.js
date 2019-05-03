aedd31ecbaee489810ad6c78576fe1ef772d7aa0
JienanChen <47368928+JienanChen@users.noreply.github.com>
2019-05-03 17:06:07 -0600

    Changes to Width and Height

--- Playing a Video/sketch.js
+++ Playing a Video/sketch.js
@@ -1,15 +1,26 @@
-// Project Title
-// Your Name
-// Date
+// Playing a Video
+// Jienan Chen
+// May 3, 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let chap;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
+  chap = createVideo("skating.mp4");
+  chap.hide(); // by default video shows up in separate dom
+  // element. hide it and draw it to the canvas
+  // instead
 }
 
 function draw() {
-  background(220);
+  background(0);
+  imageMode(CENTER);
+  image(chap, width/2, height/2); // draw the video frame to canvas
 }
+
+ function mousePressed() {
+   chap.play(); // set the video to loop and start playing
+ }
