476221bf1bfb71b8580742bbcf61e0275f844d06
JienanChen <47368928+JienanChen@users.noreply.github.com>
2019-05-03 00:34:24 -0600

    Code copied from p5js

--- Placing an Image in a Boundary/sketch.js
+++ Placing an Image in a Boundary/sketch.js
@@ -1,15 +1,29 @@
-// Project Title
-// Your Name
-// Date
+// Placing and Image Within a Boundary
+// Jienan Chen
+// May 3, 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+let chap;
+let rectWidth, rectHeight;
+
+function preload(){
+  chap = loadImage("assets/verdoux.jpg");
+  rectWidth = 345;
+  rectHeight = 220;
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  strokeWeight(13);
+  rect(15, 15, rectWidth, rectHeight);
+  image(chap, 15, 15);
+  chap.resize(rectWidth, rectHeight);
+  
 }
