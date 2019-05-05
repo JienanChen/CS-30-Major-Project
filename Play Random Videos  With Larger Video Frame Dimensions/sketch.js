2f6e909872ceca96caa666c2569a16326c1f787e
JienanChen <47368928+JienanChen@users.noreply.github.com>
2019-05-04 18:02:51 -0600

    Assets loaded with more films and amendment to code
    Added clips from Kid Auto Races at Venice, The Adventurer, The Tramp, Modern Times, The Circus, A King in New York, and The Great Dictator.

--- Play Random Videos  With Larger Video Frame Dimensions/sketch.js
+++ Play Random Videos  With Larger Video Frame Dimensions/sketch.js
@@ -15,6 +15,20 @@ let chicken;
 let pushed;
 let bread;
 let block;
+let barber;
+let cage;
+let cocaine;
+let couple;
+let dictators;
+let ending;
+let glass;
+let hitler;
+let machine;
+let maze;
+let skating2;
+let moustache;
+let politics;
+let puppets;
 
 let state;
 
@@ -44,7 +58,49 @@ function setup() {
 
   block = createVideo("assets/block.mp4");
   block.hide();
-  
+
+  barber = createVideo("assets/barber.mp4");
+  barber.hide();
+
+  cage = createVideo("assets/cage.mp4");
+  cage.hide();
+
+  cocaine = createVideo("assets/cocaine.mp4");
+  cocaine.hide();
+
+  couple = createVideo("assets/couple.mp4");
+  couple.hide();
+
+  dictators = createVideo("assets/dictators.mp4");
+  dictators.hide();
+
+  ending = createVideo("assets/ending.mp4");
+  ending.hide();
+
+  glass = createVideo("assets/glass.mp4");
+  glass.hide();
+
+  hitler = createVideo("assets/hitler.mp4");
+  hitler.hide();
+
+  machine = createVideo("assets/machine.mp4");
+  machine.hide();
+
+  maze = createVideo("assets/maze.mp4");
+  maze.hide();
+
+  skating2 = createVideo("assets/modern times skating.mp4");
+  skating2.hide();
+
+  moustache = createVideo("assets/moustache.mp4");
+  moustache.hide();
+
+  politics = createVideo("assets/politics.mp4");
+  politics.hide();
+
+  puppets = createVideo("assets/puppets.mp4");
+  puppets.hide();
+
   state = 0;
 }
 
@@ -103,6 +159,76 @@ function draw() {
     imageMode(CENTER);
     image(block, width/2, height/2);
   }
+  if (state === "barber"){
+    background(0);
+    imageMode(CENTER);
+    image(barber, width/2, height/2);
+  }
+  if (state === "cage"){
+    background(0);
+    imageMode(CENTER);
+    image(cage, width/2, height/2);
+  }
+  if (state === "cocaine"){
+    background(0);
+    imageMode(CENTER);
+    image(cocaine, width/2, height/2);
+  }
+  if (state === "couple"){
+    background(0);
+    imageMode(CENTER);
+    image(couple, width/2, height/2);
+  }
+  if (state === "dictators"){
+    background(0);
+    imageMode(CENTER);
+    image(dictators, width/2, height/2);
+  }
+  if (state === "ending"){
+    background(0);
+    imageMode(CENTER);
+    image(ending, width/2, height/2);
+  }
+  if (state === "glass"){
+    background(0);
+    imageMode(CENTER);
+    image(glass, width/2, height/2);
+  }
+  if (state === "hitler"){
+    background(0);
+    imageMode(CENTER);
+    image(hitler, width/2, height/2);
+  }
+  if (state === "machine"){
+    background(0);
+    imageMode(CENTER);
+    image(machine, width/2, height/2);
+  }
+  if (state === "maze"){
+    background(0);
+    imageMode(CENTER);
+    image(maze, width/2, height/2);
+  }
+  if (state === "skating 2"){
+    background(0);
+    imageMode(CENTER);
+    image(skating2, width/2, height/2);
+  }
+  if (state === "moustache"){
+    background(0);
+    imageMode(CENTER);
+    image(moustache, width/2, height/2);
+  }
+  if (state === "politics"){
+    background(0);
+    imageMode(CENTER);
+    image(politics, width/2, height/2);
+  }
+  if (state === "puppets"){
+    background(0);
+    imageMode(CENTER);
+    image(puppets, width/2, height/2);
+  }
 }
 
  function mousePressed() {
@@ -112,7 +238,7 @@ if (state === 0){
  }
 
 function chooseChaplinFilmToPlay(){
-  choices = [1, 2, 3, 4, 5, 6, 7];
+  choices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
   choose = random(choices);
   if (choose === 1){
     state = "skate";
@@ -142,6 +268,62 @@ function chooseChaplinFilmToPlay(){
     state = "block";
     block.play();
   }
+  if (choose === 8){
+    state = "barber";
+    barber.play();
+  }
+  if (choose === 9){
+    state = "cage";
+    cage.play();
+  }
+  if (choose === 10){
+    state = "cocaine";
+    cocaine.play();
+  }
+  if (choose === 11){
+    state = "couple";
+    couple.play();
+  }
+  if (choose === 12){
+    state = "dictators";
+    dictators.play();
+  }
+  if (choose === 13){
+    state = "ending";
+    ending.play();
+  }
+  if (choose === 14){
+    state = "glass";
+    glass.play();
+  }
+  if (choose === 15){
+    state = "hitler";
+    hitler.play();
+  }
+  if (choose === 16){
+    state = "machine";
+    machine.play();
+  }
+  if (choose === 17){
+    state = "maze";
+    maze.play();
+  }
+  if (choose === 18){
+    state = "skating 2";
+    skating2.play();
+  }
+  if (choose === 19){
+    state = "moustache";
+    moustache.play();
+  }
+  if (choose === 20){
+    state = "politics";
+    politics.play();
+  }
+  if (choose === 21){
+    state = "puppets";
+    puppets.play();
+  }
 }
 
 function stopVideos(){
@@ -152,6 +334,20 @@ function stopVideos(){
   cocktail.stop();
   pushed.stop();
   block.stop();
+  barber.stop();
+  cage.stop();
+  cocaine.stop();
+  couple.stop();
+  dictators.stop();
+  ending.stop();
+  glass.stop();
+  hitler.stop();
+  machine.stop();
+  maze.stop();
+  skating2.stop();
+  moustache.stop();
+  politics.stop();
+  puppets.stop();
 }
 
 function keyPressed(){
