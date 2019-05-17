c95f189fcbc598de2be4412f062d61a9778bc489
JienanChen <47368928+JienanChen@users.noreply.github.com>
2019-05-17 00:37:17 -0600

    Comment header updated, code transfered from p5js

--- Taking a Picture of the User/sketch.js
+++ Taking a Picture of the User/sketch.js
@@ -1,15 +1,52 @@
-// Project Title
-// Your Name
-// Date
-//
+// Taking a Picture of the User (Version 1)
+// Jienan Chen
+// May 16-17, 2019
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+let user;
+let button1;
+let button2;
+let state;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+  createP("");
+  let button1 = createButton("Camera On");
+  button1.mousePressed(cameraOn);
+  let button2 = createButton("Camera Off");
+  button2.mousePressed(cameraOff);
+  user = createCapture(VIDEO);
+  user.hide();
+  
+  let state = 0; 
 }
 
 function draw() {
   background(220);
+  if (state === 1){
+    cameraOn();
+  }
+  if (state === 2){
+    cameraOff();
+  }
+}
+
+function cameraOn(){
+  state = 1;
+  image(user, 0, 0, width, height);
+}
+
+function cameraOff(){
+  state = 2;
+  clear();
+}
+
+function mousePressed(){
+  if (state === 1 && mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height){
+    let today = day();
+    let time = millis();
+    saveCanvas("p5photo" + today + time, "jpg");
+  }
 }
