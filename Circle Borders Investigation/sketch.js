2f4f05717902d9ffa756b3fd3c862641c6c11467
JienanChen <47368928+JienanChen@users.noreply.github.com>
2019-05-22 23:08:11 -0600

    Code transfered from p5js web editor.

--- Circle Borders Investigation/sketch.js
+++ Circle Borders Investigation/sketch.js
@@ -1,15 +1,32 @@
-// Project Title
-// Your Name
-// Date
+// Circle Borders Investigation
+// Jienan Chen
+// May 22, 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  ellipseMode(CENTER);
+  ellipse(width/2, height/2, 200, 200);
+  
+  //center
+  ellipse(width/2, height/2, 30, 30);
+  
+  //left
+  ellipse(width/2 -100, height/2, 30, 30)
+  
+  //right
+  ellipse(width/2 + 100 , height/2, 30, 30)
+  
+  //up
+  ellipse(width/2, height/2 - 100, 30, 30);
+  
+  //down
+  ellipse(width/2, height/2 + 100, 30, 30);
 }
