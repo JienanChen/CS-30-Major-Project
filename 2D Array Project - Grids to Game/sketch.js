e009f4b1f6d46491b355bf4e8fac0a67389ad64f
JienanChen <47368928+JienanChen@users.noreply.github.com>
2019-06-14 20:25:27 -0600

    Distractions Class Transfered

--- 2D Array Project - Grids to Game/sketch.js
+++ 2D Array Project - Grids to Game/sketch.js
@@ -5,6 +5,52 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+class Distractions {
+  constructor(x, y, someWidth,someColour, someImage, someSpeed){
+    this.x = x;
+    this.y = y;
+    this.width = someWidth;
+    this.stroke = someColour;
+    this.img = someImage;
+    this.speed = someSpeed;
+  }
+
+  display() {
+    rectMode(CENTER);
+    imageMode(CENTER);
+    strokeWeight(7);
+    stroke(this.stroke);
+    rect(this.x, this.y, this.width, this.width - 7);
+    image(this.img, this.x, this.y, this.width, this.width - 7);
+  }
+
+  move() {
+    let choice = random(100);
+    if (this.x + this.width >= width || this.x <= 0){
+      this.x = random(0, width - this.width);
+    }
+    if (this.y + this.width - 7 >= height || this.y <= 0){
+      this.y = random(0,  height - this.width - 7);
+    }
+    if (choice < 25) {
+      //up
+      this.y -= this.speed* random(8);
+    }
+    else if (choice < 50) {
+      //down
+      this.y += this.speed* random(10);
+    }
+    else if (choice < 75) {
+      //left
+      this.x -= this.speed * random(8);
+    }
+    else {
+      //right
+      this.x += this.speed * random(10);
+    }
+  }
+}
+
 let state;
 
 //Button related global variables(by Pouya)
@@ -139,6 +185,11 @@ function draw() {
     background(chooseStateBackground);
     drawChoiceButtons();
   }
+
+  if (state === "Tag"){
+
+  }
+
   
 }
 
